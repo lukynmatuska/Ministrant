b692c6aaf276a152fb87f6544cb10e98a1f9c5e2
Solumath <solumath99@gmail.com>
2022-09-21 21:50:14 +0200

    lint and moving messages to one file

---
+++ config/messages.py
@@ -0,0 +1,4 @@
+class Messages:
+    pray_string = "Yeete náš, jenž jsi na nebesích,\ngde body tvé.\ngde opravené písemky tvé.\n\nBuď body tvé jako v nebi, tak i na zemi.\nŘešení naše vzorové dej nám dnes.\n\nA odpusť nám naše chyby,\njako i my odpouštíme tvoje na cviku.\nA neuveď nás v opakování,\nale zbav nás od Fka.\nAmen RNGesus"
+    uhoh_brief = "Ukáže počet uhoh od spuštění."
+    
