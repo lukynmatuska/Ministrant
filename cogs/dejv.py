443fff721a434491d6affc722bef1ab6bcbd5c67
Martin Laštovica <vonakom@gmail.com>
2022-09-23 00:31:03 +0200

    Update dejv.py

--- cogs/dejv.py
+++ cogs/dejv.py
@@ -4,10 +4,11 @@ from disnake.ext import commands
 from config.messages import Messages
 
 DEJV_IMAGE_ROUTE = "./assets/dejvOpravar.png"
-VUTIS_STRINGS = ["vutis","vut is","studis"]
+VUTIS_STRINGS = ["vutis", "vut is", "studis"]
 SADVID_EMOTE_ID_STRING = "<:sadvid:976410247105241098>"
 
-#class definition
+# class definition
+
 
 class Dejv(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -17,7 +18,7 @@ class Dejv(commands.Cog):
     async def on_message(self, message: Message):
 
         nalezeno = False
-        
+
         """
         When message with specific content from activation_strings
         is detected the bot sends response.
@@ -26,7 +27,7 @@ class Dejv(commands.Cog):
         for string in Messages.activation_strings:
             if string in message.content.lower():
                 nalezeno = True
-                break;
+                break
 
         if nalezeno:
 
@@ -35,11 +36,11 @@ class Dejv(commands.Cog):
             for string in VUTIS_STRINGS:
                 if string in message.content.lower():
                     nalezeno = True
-                    break;
-            
+                    break
+
             if nalezeno:
                 await message.reply("VUT IS ani Dejv nespraví " + SADVID_EMOTE_ID_STRING)
-                    
+                
             else:
                 with open(DEJV_IMAGE_ROUTE, "rb") as f:
                     await message.reply(
