8ec9639d0259237c023d54f852b83451ae84ca52
Solumath <solumath99@gmail.com>
2022-10-06 14:45:43 +0200

    rewrite dejv cog
    use more python like style
    remove czech vars :)

--- cogs/dejv.py
+++ cogs/dejv.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 from disnake import File, Message
 from disnake.ext import commands
 from config.messages import Messages
@@ -17,31 +17,16 @@ class Dejv(commands.Cog):
         is detected the bot sends response.
         """
 
-        nalezeno = False
+        message_content = message.content.lower()
 
         for string in Messages.activation_strings:
-            if string in message.content.lower():
-                nalezeno = True
-                break
-
-        if nalezeno:
-
-            nalezeno = False
-
-            for string in Messages.vutis_strings:
-                if string in message.content.lower():
-                    nalezeno = True
-                    break
-
-            if nalezeno:
-                await message.reply(Messages.dejv_vut_is)
-
-            else:
+            if string in message_content:
+                for string in Messages.vutis_strings:
+                    if string in message_content:
+                        await message.reply(Messages.dejv_vut_is)
+                        return
                 with open(DEJV_IMAGE_ROUTE, "rb") as f:
-                    await message.reply(
-                        Messages.dejv_repair_strings[randint(0, len(Messages.dejv_repair_strings)-1)],
-                        file=File(fp=f,)
-                    )
+                    await message.reply(random.choice(Messages.dejv_repair_strings), file=File(f))
 
 
 def setup(bot: commands.Bot):
